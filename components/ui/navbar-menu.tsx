7c53f822916d30f5be47082ca6a4bd202b720937
Nived <nivuyt789@gmail.com>
2025-04-24 12:50:59 +0530

    search bar implementation

--- components/ui/navbar-menu.tsx
+++ components/ui/navbar-menu.tsx
@@ -1,11 +1,12 @@
 "use client";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import Link, { LinkProps } from "next/link";
 import Image from "next/image";
 import arrow from "@/public/assets/img/home/arrow.svg";
 import { FaChevronRight } from "react-icons/fa6";
 import { IoSearchOutline } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 
 const transition = {
   type: "spring",
@@ -31,6 +32,7 @@ export const MenuItem = ({
   children?: React.ReactNode;
   noMenu?: boolean;
 }) => {
+
   return (
     <div
       onMouseEnter={() => (noMenu ? setActive(null) : setActive(item))}
@@ -77,40 +79,106 @@ export const Menu = ({
   setActive: (item: string | null) => void;
   children: React.ReactNode;
 }) => {
-  return (
-    <nav
-      onMouseLeave={() => setActive(null)} // resets the state
-      className="relative  dark:bg-black dark:border-white/[0.2] shadow-input flex justify-center items-center  h-full">
-      <div className="left-spacing pr-[20px] xxl:pr-[20px] xxxl:pr-[50px] w-full flex items-center justify-between  py-[25px] border-r border-black/10">
-        <div className="flex items-center">
-          <Link href="/">
-            <Image
-              src="/assets/img/logo.svg"
-              alt="Crest Logo"
-              width={80}
-              height={50}
-              className="h-[34px] w-auto"
-            />
-          </Link>
-        </div>
-        <div className="flex space-x-[25px] xxl:space-x-[20px] xxxl:space-x-[50px] items-center">
-          <div className="flex space-x-[15px] xxl:space-x-[20px] xxxl:space-x-[30px] items-center group">
-          {children}
-          </div>
-          <Link
-            href="contact"
-            className="self-start text-white bg-secondary hover:bg-primary group rounded-full text-xs font-normal transition duration-300 ease-in-out uppercase flex items-center justify-center gap-[15px] py-[11px] px-[19px] h-48px">
 
-            Contact <div className="rounded-full w-[18.5px] h-[20px] text-secondary bg-primary group-hover:bg-secondary group-hover:text-primary flex items-center text-[14px] justify-center transition duration-300 ease-in-out"><FaChevronRight /></div>
-          </Link>
+  const [searchActive, setSearchActive] = useState(false);
+  const searchRef = useRef<HTMLDivElement>(null);
+  const searchButtonRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (searchButtonRef.current) {
+      return;
+    }
+    
+  
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        searchRef.current &&
+        event.target instanceof Node &&
+        !searchRef.current.contains(event.target)
+      ) {
+        setSearchActive(false); // close the dropdown
+      }
+    }
+  
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="relative">
+      <nav
+        onMouseLeave={() => setActive(null)} // resets the state
+        className="relative bg-white z-50 dark:bg-black dark:border-white/[0.2] shadow-input flex justify-center items-center  h-full">
+        <div className="left-spacing pr-[20px] xxl:pr-[20px] xxxl:pr-[50px] w-full flex items-center justify-between  py-[25px] border-r border-black/10">
+          <div className="flex items-center">
+            <Link href="/">
+              <Image
+                src="/assets/img/logo.svg"
+                alt="Crest Logo"
+                width={80}
+                height={50}
+                className="h-[34px] w-auto"
+              />
+            </Link>
+          </div>
+          <div className="flex space-x-[25px] xxl:space-x-[20px] xxxl:space-x-[50px] items-center">
+            <div className="flex space-x-[15px] xxl:space-x-[20px] xxxl:space-x-[30px] items-center group">
+              {children}
+            </div>
+            <Link
+              href="contact"
+              className="self-start text-white bg-secondary hover:bg-primary group rounded-full text-xs font-normal transition duration-300 ease-in-out uppercase flex items-center justify-center gap-[15px] py-[11px] px-[19px] h-48px">
+
+              Contact <div className="rounded-full w-[18.5px] h-[20px] text-secondary bg-primary group-hover:bg-secondary group-hover:text-primary flex items-center text-[14px] justify-center transition duration-300 ease-in-out"><FaChevronRight /></div>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="px-[20px] xxl:px-[20px] xxxl:px-[50px]">
-        <div className="cins w-[48px] h-[48px] flex items-center justify-center border border-[#1F1F1F] rounded-full text-center">
-      <IoSearchOutline className="text-sm text-secondary" />
-      </div>
-      </div>
-    </nav>
+        <div className="px-[20px] xxl:px-[20px] xxxl:px-[50px]">
+          <div className="cins w-[48px] h-[48px] flex items-center justify-center border border-[#1F1F1F] rounded-full text-center cursor-pointer" ref={searchButtonRef} onClick={()=>setSearchActive(!searchActive)}>
+            {searchActive ? <IoClose className="text-sm text-secondary"/> : <IoSearchOutline className="text-sm text-secondary"/>}
+          </div>
+        </div>
+      </nav>
+      
+        <>
+        <div className={`fixed inset-0 bg-black/30 backdrop-blur-sm z-10 h-screen w-full duration-300 ${searchActive ? "translate-y-[0%]" : "translate-y-[-100%]"}`}></div>
+        <div  ref={searchRef} className={`w-full bg-white z-10 h-[500px] shadow-xl absolute top-24 right-0 duration-300 ${searchActive ? "translate-y-[0%]" : "translate-y-[-100%]"}`}>
+        <div className="container">
+          {/* <div className="absolute top-[20px] xxxl:right-[60px] right-[30px]" onClick={() => setSearchActive(!searchActive)}>
+            <IoClose className="text-lg text-green-950 cursor-pointer" />    
+          </div> */}
+
+          <form className="w-[95%] mt-3 px-2">
+            {/* <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label> */}
+            <div className="relative mt-10">
+
+              <input type="text" id="" className="outline-none block w-full p-2  text-sm text-black bg-transparent  placeholder:text-green-950 border-b" placeholder="Search Website" required />
+              <div className="absolute inset-y-0 end-0 flex items-center ps-3  cursor-pointer">
+                <svg className="w-4 h-4 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg>
+              </div>
+            </div>
+          </form>
+
+          <div className="mt-5 px-4 flex flex-col gap-5">
+            <div className="text-md font-semibold">Results</div>
+            <ul className="grid grid-cols-2 list-disc gap-5 text-xs px-4">
+              <li className="cursor-pointer">Item 1</li>
+              <li className="cursor-pointer">Item 2</li>
+              <li className="cursor-pointer">Item 3</li>
+              <li className="cursor-pointer">Item 4</li>
+            </ul>
+          </div>
+
+        </div></div>
+        </>
+
+
+    </div>
+
   );
 };
 
