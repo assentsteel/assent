9eda726a734e3e290385f6cea5e792a7e5eff368
jishnum007 <jishnu@globalsurf.ae>
2025-05-14 17:03:30 +0530

    bugfixes

--- app/component/AccreditationsList/sections/AccreditationsList.tsx
+++ app/component/AccreditationsList/sections/AccreditationsList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import Image, { StaticImageData } from "next/image";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
