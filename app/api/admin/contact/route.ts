4f7bfc4925bedf6d57e551476684c76d3bb5c08e
Nived <nived@globalsurf.ae>
2025-06-06 10:07:37 +0530

    backend console dev 2

---
+++ app/api/admin/contact/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/lib/mongodb";
+import Contact from "@/app/models/Contact";
+import { verifyAdmin } from "@/lib/verifyAdmin";
+
+export async function PATCH(req:NextRequest) {
+    try {
+        await connectDB();
+        const isAdmin = await verifyAdmin(req);
+        if(!isAdmin){
+            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+        }
+        const body = await req.json();
+        const contact = await Contact.findOneAndUpdate({}, body, { upsert: true });
+        if(contact){
+            return NextResponse.json({ message: "Details saved successfully" }, { status: 200 });
+        }else{
+            return NextResponse.json({ message: "Error saving  details" }, { status: 500 });
+        }
+    } catch (error) {
+        console.log("Error saving team details", error);
+        return NextResponse.json({ message: "Error saving team details" }, { status: 500 });
+    }
+}
+
+export async function GET() {
+    try {
+        await connectDB();
+        const contact = await Contact.findOne({});
+        if(contact){
+            return NextResponse.json({ success: true, data: contact }, { status: 200 });
+        }else{
+            return NextResponse.json({ success: false, message: "Error fetching details" }, { status: 500 });
+        }
+    } catch (error) {
+        console.log("Error fetching details", error);
+        return NextResponse.json({ success: false, message: "Error fetching details" }, { status: 500 });
+    }
+}
