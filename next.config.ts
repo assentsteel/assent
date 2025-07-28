e09f9c86dc6ca4ee6fd07cc9d4ead7d6865523d1
Nived <nived@globalsurf.ae>
2025-07-28 11:15:26 +0530

    fix:canvas server side issue

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,17 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG:true,
     unoptimized:true,
     domains: ["dl.dropboxusercontent.com","plus.unsplash.com"] // Add Dropbox domain here
-  }
+  },
+  compiler:{
+    removeConsole : process.env.NODE_ENV === 'production'
+  },
+  webpack(config, { nextRuntime }) {
+    if (nextRuntime === "nodejs") {
+      config.resolve.alias.canvas = false;
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
