061d8353ee5d7a5c6da21e8ad73498563dd80cea
jishnum007 <jishnu@globalsurf.ae>
2026-02-19 11:54:10 +0530

    build fix

--- app/component/AustralianProjects/sections/HeadingText.tsx
+++ app/component/AustralianProjects/sections/HeadingText.tsx
@@ -49,10 +49,7 @@ gsap.registerPlugin(ScrollTrigger);
       });
     }
   }, []);
-  const fadeInUp = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } },
-  };
+ 
   return (
     <section className="pt100  overflow-hidden relative  ">
       <div className="container">
