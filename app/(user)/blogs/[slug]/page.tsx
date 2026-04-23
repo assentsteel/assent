20c186259f7a09c960a1c706722fe7fd6ddc3cda
rahees <raheez1995india@gmail.com>
2026-04-23 15:24:46 +0530

    fix: update NO_INDEX_SLUGS for accurate SEO handling

--- app/(user)/blogs/[slug]/page.tsx
+++ app/(user)/blogs/[slug]/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from "next/navigation"
 import type { Metadata } from "next"
 import Script from "next/script"
 
-const NO_INDEX_SLUGS = ["globalsurf-post-lourve", "global-surf", "why-steel-erection-projects-get-delayed-how-to-avoid-it","uae-steel-fabricator-procurement-checklist"]
+const NO_INDEX_SLUGS = ["globalsurf-post-lourve", "global-surf", "why-steel-erection-projects-get-delayed-how-to-avoid-it","how-to-choose-a-steel-fabricator-in-the-uae-a-procurement-checklist"]
 
 export async function generateMetadata({
   params,
